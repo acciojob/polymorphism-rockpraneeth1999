91bb5b3e4a8965175dee2a4a464037a4a30de91f
Rock <rockpraneeth1999@gmail.com>
2023-08-09 23:39:48 +0530

    Call methods and print Attempt 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -18,8 +18,8 @@ public class Main {
 
     public static void main(String[] args) {
         Product p=new Product();
-        p.product(10,20);
-        p.product(10,20,30);
-        p.product(10.1,20.2);
+        System.out.println(p.product(10,20));
+        System.out.println(p.product(10,20,30));
+        System.out.println(p.product(10.1,20.2));
     }
 }
